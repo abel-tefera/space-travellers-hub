34d0915b873b3071db0e7c444af9f58b0cf7e740
abel-tefera <abeltefera16@gmail.com>
2023-05-15 11:39:15 +0300

    linters fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-import { colors as defaultColors } from "tailwindcss/defaultTheme";
+import { colors as defaultColors } from 'tailwindcss/defaultTheme';
 
 module.exports = {
-  content: ["./public/*.html", "./src/**/*.js"],
+  content: ['./public/*.html', './src/**/*.js'],
   theme: {
     extend: {},
   },
