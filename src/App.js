658792f72ebd846fe6c121a8e31efd39a0cfc8df
abel-tefera <abeltefera16@gmail.com>
2023-05-15 15:44:22 +0300

    set up react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Rockets />} />
+            <Route path="missions" element={<Missions />} />
+            <Route path="profile" element={<Profile />} />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
