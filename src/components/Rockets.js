e96028b280f6633293576796d0d1220dd23c5ea8
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-16 17:28:17 -0400

    Update Rockers component

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRocket } from '../redux/features/rocket/rocketSlice';
 
-const Rockets = () => (
-  <div>Rockets</div>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const { rockets } = useSelector((state) => state.rocket);
+  useEffect(() => {
+    dispatch(fetchRocket());
+  });
+
+  return (
+    <div>
+      {rockets.map((rocket) => <>{rocket.description}</>)}
+    </div>
+  );
+};
 
 export default Rockets;
