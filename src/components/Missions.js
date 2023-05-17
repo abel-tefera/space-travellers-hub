0a1f465081f36b49ca45873a743a7461131dda55
abel-tefera <abeltefera16@gmail.com>
2023-05-17 09:39:04 +0300

    styling for missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -24,15 +24,15 @@ const Missions = () => {
   return (
     <>
       <div className="container flex flex-col">
-        <div className="overflow-x-auto">
+        <div className="overflow-x-scroll">
           <div className="p-1.5 w-full inline-block align-middle">
-            <div className="overflow-hidden border rounded-lg">
+            <div className="border rounded-lg">
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-50">
                   <tr>
                     <th
                       scope="col"
-                      className="p-4 text-xs font-bold uppercase text-start"
+                      className="p-4 text-xs font-bold uppercase text-start w-1/6"
                     >
                       Mission
                     </th>
@@ -48,7 +48,12 @@ const Missions = () => {
                     >
                       Status
                     </th>
-                    <th scope="col" />
+                    <th
+                      className="p-4 text-xs font-bold text-start uppercase "
+                      scope="col"
+                    >
+                      Action
+                    </th>
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200">
@@ -60,32 +65,32 @@ const Missions = () => {
                         <td className="p-4 text-sm text-gray-800 font-bold">
                           {mission_name}
                         </td>
-                        <td className="p-4 text-sm text-gray-800">
+                        <td className="p-4 text-sm text-gray-800 text-justify">
                           {description}
                         </td>
                         <td className="p-4 text-sm font-medium ">
-                          <a
+                          <div
                             className={
                               status
-                                ? 'capitalize bg-cyan-500 text-white p-1 rounded'
-                                : 'bg-gray-500 uppercase text-white p-1 rounded'
+                                ? 'capitalize bg-cyan-500 text-white p-1 rounded cursor-pointer w-fit'
+                                : 'bg-gray-500 uppercase text-white p-1 rounded cursor-pointer w-fit'
                             }
                             href="#"
                           >
                             {status ? 'Active Member' : 'Not a member'}
-                          </a>
+                          </div>
                         </td>
                         <td className="text-sm font-medium ">
-                          <a
+                          <div
                             className={
                               status
-                                ? 'text-red-500 outline-red-500  outline p-2'
-                                : 'text-gray-500 outline-gray-500  outline p-2'
+                                ? 'text-red-500 outline-red-500  outline p-2 cursor-pointer w-fit'
+                                : 'text-gray-500 outline-gray-500  outline p-2 cursor-pointer w-fit'
                             }
                             href="#"
                           >
                             {status ? 'Leave Mission' : 'Join Mission'}
-                          </a>
+                          </div>
                         </td>
                       </tr>
                     ),
