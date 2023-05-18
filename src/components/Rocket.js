95351f83a5add499d36a29ade7ab28ec0a40b801
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-18 04:42:24 -0400

    Replace React-Bootstrap components with their equivalent HTML tags

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,7 +1,3 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Badge from 'react-bootstrap/Badge';
-
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { reserveRocket, cancelReserve } from '../redux/rockets/rocketSlice';
@@ -14,44 +10,46 @@ const Rocket = ({
   return (
     <div className="flex m-8">
       <div className="w-72">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={`${img}`} alt={name} />
-        </Card>
+        <div style={{ width: '18rem' }}>
+          <img src={`${img}`} alt={name} />
+        </div>
       </div>
 
       <div className="flex-auto mx-5">
-        <Card.Body>
-          <Card.Title className="font-bold">{name}</Card.Title>
+        <div>
+          <h1 className="font-bold">{name}</h1>
           <div className="mt-3 ">
             {reserved && (
               <div>
-                <Badge className="inline-block p-1 mr-3 bg-blue-500 text-white text-xs rounded-md">
+                <button type="button" className="inline-block p-1 mr-3 bg-blue-500 text-white text-xs rounded-md">
                   Reserved
-                </Badge>
+                </button>
                 {description}
 
-                <Button
+                <button
+                  type="button"
                   className="block p-2 mt-3 rounded-md border border-black border-opacity-40 bg-white text-black text-opacity-50"
                   onClick={() => dispatch(cancelReserve(id))}
                 >
                   Cancel Reservation
-                </Button>
+                </button>
               </div>
             )}
 
             {!reserved && (
               <div>
                 <div>{description}</div>
-                <Button
+                <button
+                  type="button"
                   className="p-2 mt-3 rounded-md bg-blue-500 text-white"
                   onClick={() => dispatch(reserveRocket(id))}
                 >
                   Reserve Rocket
-                </Button>
+                </button>
               </div>
             )}
           </div>
-        </Card.Body>
+        </div>
       </div>
     </div>
   );
