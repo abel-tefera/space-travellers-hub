fd76f3060513cb18384aba240a8d654f55498eb0
abel-tefera <abeltefera16@gmail.com>
2023-05-18 13:38:30 +0300

    my profile missions redux done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectAllMyMissions } from '../redux/missions/missionsSlice.js';
 
-const Profile = () => (
-  <div>Profile</div>
-);
+const Profile = () => {
+  const MissionData = useSelector(selectAllMyMissions);
+
+  return (
+    <div className="container flex flex-row">
+      <div className="flex flex-col">
+        {MissionData.map(({ mission_name }) => (
+          <div className="p-6">{mission_name}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
