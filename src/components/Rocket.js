eb262c9ac67831837a0cb7d8b46ccf7cec633d70
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-18 04:22:22 -0400

    Add button cancellation

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,24 +1,49 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { reserveRocket, cancelReserve } from '../redux/rockets/rocketSlice';
 
 const Rocket = ({
-  id, name, description, img, type,
-}) => (
-  <div className="flex m-8">
-    <div className="w-72">
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={`${img}`} />
-      </Card>
-    </div>
-    <div className="flex-auto mx-5">
-      <Card.Body>
-        <Card.Title className="text-l">{name}</Card.Title>
-        <Card.Text className="mt-3">{description}</Card.Text>
-        <Button className="p-2 mt-3 rounded-md bg-blue-500 text-white">Reserve Rocket</Button>
-      </Card.Body>
-    </div>
-  </div>
+  id, name, description, img, type, reserved,
+}) => {
+  const dispatch = useDispatch();
 
-);
+  return (
+    <div className="flex m-8">
+      <div className="w-72">
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={`${img}`} alt={name} />
+        </Card>
+      </div>
+
+      <div className="flex-auto mx-5">
+        <Card.Body>
+          <Card.Title className="font-bold">{name}</Card.Title>
+          <div className="mt-3 ">
+            {reserved && (
+              <div>
+                <Badge className="inline-block p-1 mr-3 bg-blue-500 text-white text-xs rounded-md">
+                  Reserved
+                </Badge>
+                {description}
+
+                <Button
+                  className="block p-2 mt-3 rounded-md border border-black border-opacity-40 bg-white text-black text-opacity-50"
+                  onClick={() => dispatch(cancelReserve(id))}
+                >
+                  Cancel Reservation
+                </Button>
+              </div>
+            )}
+
+          </div>
+        </Card.Body>
+      </div>
+    </div>
+  );
+};
 
 export default Rocket;
